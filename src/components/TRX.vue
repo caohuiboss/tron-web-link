b21b3b958d65df47874953c84524fc42fcdcf1eb
hui.cao <hui.cao@fargowealth.com.hk>
2021-12-21 10:39:47 +0800

    新增不同数量批量转账

--- src/components/TRX.vue
+++ src/components/TRX.vue
@@ -20,9 +20,10 @@
         <el-radio-group v-model="formData.way">
           <el-radio :label="0">单个转账</el-radio>
           <el-radio :label="1">批量转账</el-radio>
+          <el-radio :label="2">不同数量批量转账</el-radio>
         </el-radio-group>
       </el-form-item>
-      <el-form-item label="转账数量">
+      <el-form-item label="转账数量" v-if="formData.way !== 2">
         <el-input-number v-model="formData.num" :min="0"></el-input-number>
       </el-form-item>
       <el-form-item v-if="formData.way === 0" label="收款人地址(单个地址)">
@@ -39,12 +40,12 @@
 </template>
 
 <script setup>
-import { ElMessage } from "element-plus";
-import { onMounted, reactive, ref, watch } from "vue";
+import { ElMessage } from "element-plus"
+import { onMounted, reactive, ref, watch } from "vue"
 
-const CONTANS = 1000000;
-const ownerAddress = ref();
-const tronWeb = ref(null);
+const CONTANS = 1000000
+const ownerAddress = ref()
+const tronWeb = ref(null)
 
 const formData = reactive({
   contractAddress: "TRTSKHtGuvX2rQeQLgtkfya1MYgidHmsmd",
@@ -52,14 +53,14 @@ const formData = reactive({
   num: 0.2,
   currency: 1,
   receiveAddress: undefined,
-});
+})
 
 const linkWallet = () => {
   if (window.tronWeb) {
-    tronWeb.value = window.tronWeb;
-    ownerAddress.value = tronWeb.value.defaultAddress.base58;
+    tronWeb.value = window.tronWeb
+    ownerAddress.value = tronWeb.value.defaultAddress.base58
   } else {
-    ElMessage.warning("请下载波宝钱包浏览器插件");
+    ElMessage.warning("请下载波宝钱包浏览器插件")
   }
 };
 
@@ -67,63 +68,77 @@ const dealAddresses = (str) =>
   str
     .replace(/(\r\n)|(\n)/g, ",")
     .split(",")
-    .filter((v) => v && v.trim());
+    .filter((v) => v && v.trim())
 
 
 // trx转账交易
 const transactionTrx = async (receiveAddress) => {
   const tx = await tronWeb.value.transactionBuilder.sendTrx(
     receiveAddress, formData.num * CONTANS, ownerAddress.value
-  );
-  const signedTx = await tronWeb.value.trx.sign(tx);
-  const broastTx = await tronWeb.value.trx.sendRawTransaction(signedTx);
-  console.log("TRX发放结果: ", receiveAddress);
+  )
+  const signedTx = await tronWeb.value.trx.sign(tx)
+  const broastTx = await tronWeb.value.trx.sendRawTransaction(signedTx)
+  console.log("TRX发放结果: ", receiveAddress)
   console.log(broastTx)
 }
 
-// 代币转
-const transactionToken = async (receiveAddress) => {
+/**
+ * 代币转账
+ * @param {String} receiveAddress 需要接收的地址
+ * @param {Number} num  如果有 num ，代表自定义数量转账
+ */
+const transactionToken = async (receiveAddress, num = 0) => {
   const parameter = [
     { type: "address", value: receiveAddress },
-    { type: "uint256", value: formData.num * CONTANS },
-  ];
-  var tx = await tronWeb.value.transactionBuilder.triggerSmartContract(
+    { type: "uint256", value: num ? parseInt(num * CONTANS) : formData.num * CONTANS }
+  ]
+
+  const tx = await tronWeb.value.transactionBuilder.triggerSmartContract(
     formData.contractAddress,
     "transfer(address,uint256)",
     {},
     parameter,
     ownerAddress.value
-  );
-  var signedTx = await tronWeb.value.trx.sign(tx.transaction);
-  var broastTx = await tronWeb.value.trx.sendRawTransaction(signedTx);
-  console.log("代币发放结果: ", receiveAddress);
+  )
+
+  const signedTx = await tronWeb.value.trx.sign(tx.transaction)
+  const broastTx = await tronWeb.value.trx.sendRawTransaction(signedTx)
+  console.log("代币发放结果: ", receiveAddress, num || formData.num)
   console.log(broastTx)
-  let contract = await tronWeb.value.contract().at(formData.contractAddress);
-  await contract.decimals().call();
+  const contract = await tronWeb.value.contract().at(formData.contractAddress)
+  await contract.decimals().call()
 };
 
-// 判断转啥币
+// 判断转代币还是TRX
 const transaction = async (receiveAddress) => {
   if (formData.currency === 0) {
-    await transactionTrx(receiveAddress);
+    await transactionTrx(receiveAddress)
   } else {
-    await transactionToken(receiveAddress);
+    await transactionToken(receiveAddress)
   }
 }
 
 const onSubmit = async () => {
   if (!formData.receiveAddress) {
     ElMessage.warning("请填写转账地址");
-    return;
+    return
   }
+
   if (formData.way === 0) {
     await transaction(formData.receiveAddress)
-  } else {
-    const receiveAddress = dealAddresses(formData.receiveAddress);
+  } else if (formData.way === 1) {
+    const receiveAddress = dealAddresses(formData.receiveAddress)
     for (let index = 0; index < receiveAddress.length; index++) {
-      await transaction(receiveAddress[index]);
+      await transaction(receiveAddress[index])
+    }
+  } else {
+    const receiveAddress = dealAddresses(formData.receiveAddress)
+    for (let index = 0; index < receiveAddress.length; index++) {
+      const splitAddress = receiveAddress[index].split('\t')
+      await transactionToken(splitAddress[0], splitAddress[1])
     }
   }
+  console.log('币已经全部发完了')
 };
 
 watch(() => [formData.currency, formData.way], () => {
@@ -131,13 +146,12 @@ watch(() => [formData.currency, formData.way], () => {
 })
 
 const clearFormData = () => {
-  // formData.contractAddress = undefined
   formData.receiveAddress = undefined
 }
 
 onMounted(() => {
   if (window.tronWeb) {
-    tronWeb.value = window.tronWeb;
+    tronWeb.value = window.tronWeb
   }
 });
 </script>
